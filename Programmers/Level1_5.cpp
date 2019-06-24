a05ee5676dc96bf20f8c2eb52ae125ea1ad5a6d0
eojine <eozinee@gmail.com>
2019-06-24 18:57:46 +0900

    체육복
    Greedy

---
+++ Programmers/Level1_5.cpp
@@ -0,0 +1,41 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, vector<int> lost, vector<int> reserve) {
+    int answer = 0;
+    vector<int> students(n, 1);
+
+    for (int i = 0; i < lost.size(); ++i)
+        students[lost[i] - 1]--;
+
+    for (int i = 0; i < reserve.size(); ++i)
+        students[reserve[i] - 1]++;
+
+    for (int i = 0; i < n; i++) {
+        if (i != 0 && students[i] == 0) {
+            if (students[i - 1] == 2) {
+                students[i]++;
+                students[i - 1]--;
+                continue;
+            }
+        }
+
+        if (i != n - 1 && students[i] == 0) {
+            if (students[i + 1] == 2) {
+                students[i]++;
+                students[i + 1]--;
+            }
+        }
+
+    }
+
+    for (int i = 0; i < n; ++i) {
+        if (students[i] > 0) {
+            answer++;
+        }
+    }
+
+    return answer;
+}
