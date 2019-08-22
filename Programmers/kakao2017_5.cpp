725d727a5291d204189afe656cd3552bd00aa993
Ella <eozinee@gmail.com>
2019-08-22 20:37:34 +0900

    [1차] 프렌즈4블록

---
+++ Programmers/kakao2017_5.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int m, int n, vector<string> board) {
+    int answer = 0;
+    bool flag = false;
+    while (!flag) {
+        vector<vector<bool>> visit(m, vector<bool>(n));
+        flag = true;
+        for (int i = 0; i < m - 1; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                if (board[i][j] == ' ')
+                    continue;
+                if ((board[i][j] == board[i][j + 1]) && (board[i][j] == board[i + 1][j]) &&
+                    (board[i][j] == board[i + 1][j + 1])) {
+                    visit[i][j] = 1;
+                    visit[i][j + 1] = 1;
+                    visit[i + 1][j] = 1;
+                    visit[i + 1][j + 1] = 1;
+                    flag = false;
+                }
+            }
+        }
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (visit[i][j] == 1) {
+                    answer++;
+                    for (int k = i - 1; k >= 0; k--) {
+                        board[k + 1][j] = board[k][j];
+                        board[k][j] = ' ';
+                    }
+                }
+            }
+        }
+    }
+    return answer;
+}
