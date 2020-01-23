d59989837d73864190f76353f8053bb249580fd8
Eojin <eozinee@gmail.com>
2020-01-24 00:57:30 +0900

    Create boj_1110.java

---
+++ BOJ_Java/boj_1110.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int NUM = num;
+        int res = 0;
+
+        while (true) {
+            int first = num / 10;
+            int second = num % 10;
+
+            int tmp = first + second;
+
+            num = ((num % 10) * 10) + tmp % 10;
+            res++;
+
+            System.out.println(num + " " + tmp);
+            if (num == NUM)
+                break;
+        }
+
+        System.out.println(res);
+    }
+}
